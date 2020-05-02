fe6279bb652190c3d0c30a816c64056aadf020c3
tgwalker93 <tgwalker93@gmail.com>
2020-05-02 16:43:40 -0700

    trying to heroku push

---
+++ client/build/precache-manifest.8ed4a919945eb1f61d77db012482b70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa53e81350059b896de241a85188a21e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b0e6afa6297cc15a1b",
+    "url": "/static/css/main.f03413d0.chunk.css"
+  },
+  {
+    "revision": "8b3b4541747aaeffc08a",
+    "url": "/static/js/2.5cb7a0e7.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.5cb7a0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b0e6afa6297cc15a1b",
+    "url": "/static/js/main.87e32fdd.chunk.js"
+  },
+  {
+    "revision": "5ab23739c4ff8e2a4c0f",
+    "url": "/static/js/runtime-main.98b6abcb.js"
+  }
+]);
